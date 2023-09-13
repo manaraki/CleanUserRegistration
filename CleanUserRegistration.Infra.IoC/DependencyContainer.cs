7893c253424adeb4f7974553786bed3d380086e5
maryam <maryam.anaraki@gmail.com>
2023-09-13 15:20:16 +0330

    add Infra.IoC Layer

---
+++ CleanUserRegistration.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,27 @@
+﻿using CleanUserRegistration.Domain.Interfaces;
+using CleanUserRegistration.Infra.Data.Context;
+using CleanUserRegistration.Infra.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanUserRegistration.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // DbContext
+            services.AddDbContext<UserContext>(options => options.UseSqlServer(connectionString:"Data Source=localhost;Initial Catalog=CleanUserDB;Integrated Security=true"));
+
+            // Infra Data Layer
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            // Application Layer
+        }
+    }
+}
