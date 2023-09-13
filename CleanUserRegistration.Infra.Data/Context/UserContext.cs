7a76deb4b64e5dbe229c7361f446460c38bc0557
maryam <maryam.anaraki@gmail.com>
2023-09-13 15:19:03 +0330

    add Infra.Data Layer

---
+++ CleanUserRegistration.Infra.Data/Context/UserContext.cs
@@ -0,0 +1,22 @@
+﻿using CleanUserRegistration.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanUserRegistration.Infra.Data.Context
+{
+    public class UserContext:DbContext
+
+    {           
+        public UserContext(DbContextOptions<UserContext> options):base(options)
+        {
+            
+        }               
+
+        public DbSet<User>? Users { get; set; }
+    }
+}
---
+++ CleanUserRegistration.Infra.Data/Repository/UserRepository.cs
@@ -0,0 +1,52 @@
+﻿using CleanUserRegistration.Domain.Interfaces;
+using CleanUserRegistration.Domain.Models;
+using CleanUserRegistration.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanUserRegistration.Infra.Data.Repository
+{
+    public class UserRepository:IUserRepository
+    {
+        private readonly UserContext _userContext;
+
+        public UserRepository(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+
+        public void Delete(User user)
+        {
+            _userContext.Entry(user).State = EntityState.Deleted;
+        }
+
+        public List<User> GetAll()
+        {
+            return _userContext.Users.ToList();
+        }
+
+        public User GetById(int id)
+        {
+            return _userContext.Users.Find(id);
+        }
+
+        public void Insert(User user)
+        {
+            _userContext.Users.Add(user);
+        }
+
+        public void Save()
+        {
+            _userContext.SaveChanges();
+        }
+
+        public void Update(User user)
+        {
+            _userContext.Entry(user).State = EntityState.Modified;
+        }
+    }
+}
