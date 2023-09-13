fc08e758cc0cd81671f5f38f24e0cd33656071c5
maryam <maryam.anaraki@gmail.com>
2023-09-13 17:34:15 +0330

    add Application Layer

---
+++ CleanUserRegistrationApplication/Interfaces/IUserRegisterService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanUserRegistrationApplication.Interfaces
+{
+    internal interface IUserRegisterService
+    {
+        public void RegisterUser(string first_name, string last_name, string email, string phone);
+    
+    }
+}
---
+++ CleanUserRegistrationApplication/Services/UserRegisterService.cs
@@ -0,0 +1,35 @@
+﻿using CleanUserRegistration.Domain.Interfaces;
+using CleanUserRegistration.Domain.Models;
+using CleanUserRegistrationApplication.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanUserRegistrationApplication.Services
+{
+    public class UserRegisterService:IUserRegisterService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserRegisterService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public void RegisterUser(string first_name, string last_name, string email, string phone)
+        {
+            User user = new User
+            {
+                first_name = first_name,
+                last_name = last_name,
+                email = email,
+                phone = phone
+            };
+
+            _userRepository.Insert(user);
+        }
+    }
+}
+
